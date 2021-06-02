2f3bf969d90ce2164105f134d5d361b285b92e01
NCJ Charlie <me@ncj.wiki>
2021-06-03 00:34:28 +0800

    Update examples.js

--- gnc-frontend/src/data/examples.js
+++ gnc-frontend/src/data/examples.js
@@ -133,5 +133,21 @@ int foo() {
     return 0;
 }
 `
+    },
+    {
+        id: 9,
+        name: 'Type System',
+        code: `int main() {
+    bool a = false;
+    byte b = 0o3;
+    short c = 0x3;
+    int d = 0b1011;
+    long f = 0xcafebabe;
+
+    float g = 0.5;
+    double h = 0.25;
+
+    return a;
+}`
     }
 ]
