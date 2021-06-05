712b24a0388f3d9823b7dfc6d928eba5bcbafce2
NCJ Charlie <me@ncj.wiki>
2021-06-05 22:12:05 +0800

    tweaks on CRA and tailwindcss
    Eliminated PostCSS purge warnings

--- gnc-frontend/tailwind.config.js
+++ gnc-frontend/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  // purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: false, // PostCSS 8 conflicts with CRA
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
